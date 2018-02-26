1c7a6716d9ad9047d34742f36291b548b1762aa5
CharlesMolino <molino.charles@gmail.com>
2018-02-26 21:59:27 +0100

    2eme commit

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -1,8 +1,91 @@
 package domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Person {
 
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String mail;
+    private List<Person> friends;
+    //private List<Home> homeList;
+
+    public Person(String lastName, String firstName, String mail){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mail = mail;
+        //this.homeList = new ArrayList<Home>();
+        this.friends = new ArrayList<Person>();
+    }
+
+    public Person(String lastName){
+        this.lastName = lastName;
+    }
+
+    public Person(){}
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public List<Person> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<Person> friends) {
+        this.friends = friends;
+    }
+
+    public void addFriend(Person p){
+        friends.add(p);
+    }
+
+    public void removeFriend(int index){
+        friends.remove(index);
+    }
+
+    public void removeFriend(Person p){
+        friends.remove(p);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
